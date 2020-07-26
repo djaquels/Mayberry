e1a3e6e22994c4b0b38c434ca476c6cbd5a38585
Daniel Jacales <hjacales@gmail.com>
2020-07-26 13:06:57 -0500

    sequelizer

---
+++ ddl.sql
@@ -0,0 +1,21 @@
+CREATE TABLE squad(id serial, name varchar(150)
+PRIMARY KEY (id)
+);
+
+
+CREATE TABLE component (
+  id serial, name varchar(150), url varchar(150),port integer, discover_name varchar(150), id_squad integer, gitlab varchar(150)
+, CONSTRAINT fk_squad
+ FOREIGN KEY(id_squad)
+ REFERENCES squad(id)
+);
+
+CREATE TABLE dependencies (
+ id serial, grade integer, id_c1 integer, id_c2 integer,
+ CONSTRAINT fk_c1
+ FOREIGN KEY(id_c1)
+ REFERENCES component(id),
+ CONSTRAINT fk_c2
+ FOREIGN KEY(id_c2)
+ REFERENCES component(id)
+);
